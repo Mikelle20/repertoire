8f1b430453221c4515a52e7174094ef08a1ff8ae
Mikelle Wade <mikellewade@gmail.com>
2022-07-26 18:33:40 -0400

    testing serving of static files

--- app.js
+++ app.js
@@ -39,5 +39,5 @@ app.listen(PORT, () => {
 })
 
 app.get('*', (req,res)=> {
-  res.sendFile(path.join(__dirname, 'client/public/index.html'))
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
 })
