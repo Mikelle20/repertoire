fcf2c0b2c6a5a5ebafa066d980ec8103ccfef8bf
Mikelle Wade <mikellewade@gmail.com>
2022-07-26 18:28:28 -0400

    fixed serving of client files

--- app.js
+++ app.js
@@ -38,13 +38,6 @@ app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`)
 })
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/public'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-  });
-}
+app.get('*', (req,res)=> {
+  res.sendFile(path.join(__dirname, 'client/public/index.html'))
+})
