ab71bfbf830a26d2148da83eaa61750beade164e
Mikelle Wade <mikellewade@gmail.com>
2022-07-26 21:57:31 -0400

    fixing serving of production assets

--- app.js
+++ app.js
@@ -37,10 +37,9 @@ app.use('/playlist', playlist)
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')))
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
